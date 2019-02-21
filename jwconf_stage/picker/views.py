7cad447c49de45fb5d1ec44e2395957dc7f047d9
Simon Zigelli <zigellsn@gmail.com>
2019-02-21 13:43:40 +0100

    Direct login if there is only one congregation

--- jwconf_stage/picker/views.py
+++ jwconf_stage/picker/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Credential
 
 
 def picker(request):
-    credentials = Credential.objects.order_by('-congregation')
+    credentials = Credential.objects.order_by('congregation')
+    if credentials.count() == 1:
+        return redirect('login', congregation=credentials[0].congregation)
     context = {'credentials': credentials}
     return render(request, "picker/tiles.html", context)
