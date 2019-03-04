132f17a0937317db16b8647c560321b052c19282
Simon Zigelli <zigellsn@gmail.com>
2019-03-04 17:21:18 +0100

    Add shutdown link

--- jwconf_stage/picker/views.py
+++ jwconf_stage/picker/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from subprocess import call
 
 from .models import Credential
 
@@ -9,3 +12,13 @@ def picker(request):
         return redirect('login:login', congregation=credentials[0].congregation)
     context = {'credentials': credentials}
     return render(request, "picker/tiles.html", context)
+
+
+def confirm_shutdown(request):
+    return render(request, "picker/shutdown.html")
+
+
+def shutdown(request):
+    call(['shutdown', '-h', 'now'], shell=False)
+    return HttpResponse("Shutdown in progress")
+
