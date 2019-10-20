a6eb02e819f63f5e276d2b048b32718fdc61ab44
Simon Zigelli <zigellsn@gmail.com>
2019-10-20 22:52:13 +0200

    Fix design, fix small bugs

--- jwconf_stage/picker/views.py
+++ jwconf_stage/picker/views.py
@@ -11,20 +11,28 @@ def picker(request):
     credentials = Credential.objects.order_by('congregation')
     if credentials.count() == 1:
         return redirect('login:login', congregation=credentials[0].congregation)
+    host_ip, host_name = get_address()
+    context = {'credentials': credentials, 'ip': host_ip, 'port': request.get_port(), 'hostname': host_name,
+               'shutdown_icon': True}
+    return render(request, "picker/tiles.html", context)
+
+
+def confirm_shutdown(request):
+    host_ip, host_name = get_address()
+    context = {'ip': host_ip, 'port': request.get_port(), 'hostname': host_name, 'shutdown_icon': False}
+    return render(request, "picker/shutdown.html", context)
+
+
+def shutdown(request):
+    call(['shutdown', '-h', 'now'], shell=False)
+    return HttpResponse("Shutdown in progress")
+
+
+def get_address():
     host_name, host_ip = '', ''
     try:
         host_name = socket.gethostname()
         host_ip = socket.gethostbyname(host_name)
     except socket.error:
         print("Unable to get Hostname and IP")
-    context = {'credentials': credentials, 'ip': host_ip, 'port': request.get_port(), 'hostname': host_name}
-    return render(request, "picker/tiles.html", context)
-
-
-def confirm_shutdown(request):
-    return render(request, "picker/shutdown.html")
-
-
-def shutdown(request):
-    call(['shutdown', '-h', 'now'], shell=False)
-    return HttpResponse("Shutdown in progress")
+    return host_ip, host_name
