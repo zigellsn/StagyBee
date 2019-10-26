2daea1c985d1f2ac75868aaf1cce66efbf479547
Simon Zigelli <zigellsn@gmail.com>
2019-10-26 20:01:11 +0200

    Eliminate shutdown.html and improve design

--- jwconf_stage/picker/urls.py
+++ jwconf_stage/picker/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'picker'
 urlpatterns = [
     path('', views.picker, name='picker'),
-    path('confirm_shutdown/', views.confirm_shutdown, name='confirm_shutdown'),
     path('shutdown/', views.shutdown, name='shutdown'),
+    path('reboot/', views.reboot, name='reboot'),
 ]
--- jwconf_stage/picker/views.py
+++ jwconf_stage/picker/views.py
@@ -12,20 +12,25 @@ def picker(request):
     if credentials.count() == 1:
         return redirect('login:login', congregation=credentials[0].congregation)
     host_ip, host_name = get_address()
+    if credentials.count() <= 2:
+        size = 3
+        col = 1
+    else:
+        size = 4
+        col = 3
     context = {'credentials': credentials, 'ip': host_ip, 'port': request.get_port(), 'hostname': host_name,
-               'shutdown_icon': True}
+               'shutdown_icon': True, 'size': size, 'col': col}
     return render(request, "picker/tiles.html", context)
-
-
-def confirm_shutdown(request):
-    host_ip, host_name = get_address()
-    context = {'ip': host_ip, 'port': request.get_port(), 'hostname': host_name, 'shutdown_icon': False}
-    return render(request, "picker/shutdown.html", context)
 
 
 def shutdown(request):
     call(['shutdown', '-h', 'now'], shell=False)
     return HttpResponse("Shutdown in progress")
+
+
+def reboot(request):
+    call(['shutdown', '-r', 'now'], shell=False)
+    return HttpResponse("Reboot in progress")
 
 
 def get_address():
