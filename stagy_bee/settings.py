7059ac780023aca0c5a17c9e4cafe2e46bceb8c1
Simon Zigelli <zigellsn@gmail.com>
2019-12-06 19:53:43 +0100

    Add decouple for DEBUG value

--- stagy_bee/settings.py
+++ stagy_bee/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("SECRET_KEY", default='abc_change_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=True)
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
