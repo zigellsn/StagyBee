c2ec44c945cf432578d9b0ff369b9ba69f632987
Simon Zigelli <zigellsn@gmail.com>
2020-02-19 23:33:33 +0100

    Enhance get_times.py

--- app/console/get_times.py
+++ app/console/get_times.py
@@ -20,7 +20,36 @@ from dateutil.relativedelta import relativedelta, MO
 PREFIX = "https://www.jw.org/en/library/jw-meeting-workbook"
 
 
-async def get_month_name(month):
+async def extract(start_date, end_date=None, language="en"):
+    last_monday = start_date + relativedelta(weekday=MO(-1))
+
+    async with aiohttp.ClientSession() as session:
+        weeks = {}
+        response_code = 200
+        while response_code == 200:
+            next_sunday = last_monday + relativedelta(days=6)
+            if last_monday.year >= 2020:
+                url = await __get_2020_url(last_monday, next_sunday)
+            else:
+                url = await __get_url(last_monday, next_sunday)
+            response_code, content = await __get_workbook(session, url)
+            if language == "en":
+                times = await __parse(content, "en")
+            else:
+                language_url = await __get_language_url(content, language)
+                response_code, content = await __get_workbook(session, language_url)
+                times = await __parse(content, language)
+            print(times)
+
+            weeks[last_monday] = times
+            last_monday = last_monday + relativedelta(days=7)
+            if end_date is not None and last_monday > end_date:
+                response_code = 404
+    await session.close()
+    return weeks
+
+
+async def __get_month_name(month):
     switcher = {
         1: "January",
         2: "February",
@@ -38,63 +67,76 @@ async def get_month_name(month):
     return switcher.get(month, "Invalid month")
 
 
-async def extract(start_date, end_date=None):
-    last_monday = start_date + relativedelta(weekday=MO(-1))
-
-    async with aiohttp.ClientSession() as session:
-        weeks = {}
-        response_code = 200
-        while response_code == 200:
-            next_sunday = last_monday + relativedelta(days=6)
-            if last_monday.year >= 2020:
-                url = await get_2020_url(last_monday, next_sunday)
-            else:
-                url = await get_url(last_monday, next_sunday)
-
-            print(url)
-            async with session.get(url) as resp:
-                response_code = resp.status
-                if resp.status == 200:
-                    print("Download completed. Parsing...")
-                    content = await resp.text()
-                    times = await parse(content)
-                    print(times)
-            weeks[last_monday] = times
-            last_monday = last_monday + relativedelta(days=7)
-            if end_date is not None and last_monday > end_date:
-                response_code = 404
-    await session.close()
-    return weeks
+async def __get_language_regex(language):
+    switcher = {
+        "en": [r"\([0-9]+(\u0020|\u00A0)min.*?\)", r"[0-9]+", ")"],
+        "de": [r"\(.*?[0-9]+(\u0020|\u00A0)Min.\)", r"[0-9]+", ")"],
+        "fr": [r"\([0-9]+(\u0020|\u00A0)min.*?\)", r"[0-9]+", ")"],
+        # "fa": [r"\(.*?‏[۱-۹]+ دقیقه\)", r"[۱-۹]+", "("], #TODO: Wrong regex.
+        "it": [r"\([0-9]+(\u0020|\u00A0)min.*?\)", r"[0-9]+", ")"],
+        "el": [r"\([0-9]+(\u0020|\u00A0)(λεπτά|λεπτό).*?\)", r"[0-9]+", ")"],
+    }
+    return switcher.get(language, "Invalid language")
 
 
-async def parse(content):
+async def __get_language_url(content, language):
+    lines = content.split("\n")
+    for line in lines:
+        if line.find(f"hreflang=\"{language}\"") != -1:
+            reg = re.compile(r"href=\".*?\"")
+            text = re.findall(reg, line)
+            if text:
+                length = len(text[0]) - 1
+                return text[0][6:length]
+    return ""
+
+
+async def __get_workbook(session, url):
+    print(url)
+    print("Fetching workbook...")
+    async with session.get(url) as resp:
+        response_code = resp.status
+        if resp.status == 200:
+            print("Download completed. Parsing...")
+            content = await resp.text()
+        return response_code, content
+
+
+async def __parse(content, language):
+    regex = await __get_language_regex(language)
     times = []
-    times_tmp = re.findall("\([0-9]+ min.*?\)", content)
-    for t in times_tmp:
-        ti = re.findall("[0-9]+", t)
-        times.append(int(ti[0]))
+    lines = content.split("\n")
+    for line in lines:
+        clean = await __clean_html(line, regex[2])
+        times_tmp = re.findall(regex[0], clean)
+        if not times_tmp:
+            continue
+        ti = re.findall(regex[1], clean)
+        if not ti:
+            continue
+        times.append([int(ti[0]), clean])
     return times
 
 
-async def get_url(last_monday, next_sunday):
-    month = await get_month_name(last_monday.month)
+async def __get_url(last_monday, next_sunday):
+    month = await __get_month_name(last_monday.month)
     if last_monday.month == next_sunday.month:
         url = f"{PREFIX}/{month.lower()}-{last_monday.year}-mwb/meeting-" \
               f"schedule-{month.lower()}{last_monday.day}-{next_sunday.day}/"
     else:
-        next_month = await get_month_name(next_sunday.month)
+        next_month = await __get_month_name(next_sunday.month)
         url = f"{PREFIX}/{month.lower()}-{last_monday.year}-mwb/meeting-" \
               f"schedule-{month.lower()}{last_monday.day}-{next_month.lower()}{next_sunday.day}/"
     return url
 
 
-async def get_2020_url(last_monday, next_sunday):
-    month = await get_month_name(last_monday.month)
+async def __get_2020_url(last_monday, next_sunday):
+    month = await __get_month_name(last_monday.month)
     if last_monday.month == next_sunday.month:
         url = f"{PREFIX}/{month.lower()}-{last_monday.year}-mwb/Our-Christian-Life-and-Ministry-" \
               f"Schedule-for-{month}-{last_monday.day}-{next_sunday.day}-{last_monday.year}/"
     else:
-        next_month = await get_month_name(next_sunday.month)
+        next_month = await __get_month_name(next_sunday.month)
         if last_monday.year == next_sunday.year:
             url = f"{PREFIX}/{month.lower()}-{last_monday.year}-mwb/Our-Christian-Life-and-Ministry-" \
                   f"Schedule-for-{month}-{last_monday.day}-{next_month}-{next_sunday.day}-{last_monday.year}/"
@@ -102,3 +144,9 @@ async def get_2020_url(last_monday, next_sunday):
             url = f"{PREFIX}/{month.lower()}-{last_monday.year}-mwb/Our-Christian-Life-and-Ministry-Schedule-" \
                   f"for-{month}-{last_monday.day}-{last_monday.year}-{next_month}-{next_sunday.day}-{next_sunday.year}/"
     return url
+
+
+async def __clean_html(raw_html, regex):
+    clean_reg = re.compile(r"<.*?>")
+    clean_text = re.sub(clean_reg, "", raw_html)
+    return clean_text[:clean_text.find(regex) + 1].strip()
