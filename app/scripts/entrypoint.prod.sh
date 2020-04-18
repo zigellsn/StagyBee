c176ab84192fa24c74f796eb69410433926b8963
Simon Zigelli <zigellsn@gmail.com>
2020-04-18 14:41:12 +0200

    Fix port in entypoint.prod.sh and entrypoint.sh

--- app/scripts/entrypoint.prod.sh
+++ app/scripts/entrypoint.prod.sh
@@ -7,7 +7,7 @@ if [ -e ".env" ]; then
       ENV_SQL_HOST="$(grep SQL_HOST .env | cut -d '=' -f2)"
       SQL_HOST="${ENV_SQL_HOST:-localhost}"
       ENV_SQL_PORT="$(grep SQL_PORT .env | cut -d '=' -f2)"
-      SQL_PORT="${ENV_SQL_PORT:-5678}"
+      SQL_PORT="${ENV_SQL_PORT:-5432}"
     fi
 fi
 
