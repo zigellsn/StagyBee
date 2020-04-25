ee705f8d7eaea18affd3f95852070e288ea1ffc0
Simon Zigelli <zigellsn@gmail.com>
2020-04-25 20:20:27 +0200

    Rework login and password reset process

--- app/stagy_bee/settings/common.py
+++ app/stagy_bee/settings/common.py
@@ -78,6 +78,14 @@ LOGIN_REDIRECT_URL = '/console/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
 
+EMAIL_HOST = config("EMAIL_HOST", default="localhost")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_PORT = config("EMAIL_PORT", default="25")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=False, cast=bool)
+EMAIL_USE_SSL = config("EMAIL_USE_SSL", default=False, cast=bool)
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="webmaster@localhost")
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
