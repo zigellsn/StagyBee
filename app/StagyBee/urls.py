4aa7a2b5351674fcfe6909c3a6d69fb63c73e939
Simon Zigelli <zigellsn@gmail.com>
2021-12-05 19:49:17 +0100

    Improve button design

--- app/StagyBee/urls.py
+++ app/StagyBee/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from django.views.generic import RedirectView
-from django.views.i18n import JavaScriptCatalog
 
 from console.views import SettingsView, StartupView
 from .views import SchemeView, ToggleSchemeView
